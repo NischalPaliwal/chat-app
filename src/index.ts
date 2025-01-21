36db964aff3f13eaf008f1b4efa1ccb0fdfe9e1e
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-01-21 20:54:25 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({
+    port: 2322
+});
+
+interface User {
+    socket: WebSocket,
+    room: string
+}
+
+let allSockets: User[] = [];
+
+wss.on('connection', (socket) => {
+
+    socket.on('message', (message) => {
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
+        if (parsedMessage.type === "chat") {
+            const currentUserRoom = allSockets.find((x) => x.socket == socket);
+            allSockets.forEach((s) => {
+                if (s.room == currentUserRoom?.room) {
+                    s.socket.send(parsedMessage.payload.message);
+                }
+            });
+        }
+    });
+});
